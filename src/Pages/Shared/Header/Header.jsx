5e5b1b0073e54b5883ccabc97ec25fa8b0dca272
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-09-11 01:53:26 +0600

    Install react router and set components layout.

---
+++ src/Pages/Shared/Header/Header.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <p>Header</p>
+        </div>
+    );
+};
+
+export default Header;
