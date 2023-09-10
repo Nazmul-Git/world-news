c76b9986b675c722155aa935ec0ab54ad3699a5d
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-09-11 03:51:11 +0600

    Implement header & nav bar.

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import logo from '../../../assets/logo.png';
 import moment from 'moment';
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Nav,  Navbar } from 'react-bootstrap';
 import Marquee from "react-fast-marquee";
 
 const Header = () => {
@@ -13,10 +13,29 @@ const Header = () => {
                 <p>Journalism Without Fear or Favour</p>
                 <p>{moment().format("dddd, MMMM Do, YYYY, h:mm:ss a")}</p>
             </div>
-            <div>
+            <div className='d-flex  bg-success p-2 text-dark bg-opacity-25'>
                 <Button variant='danger'>Latest</Button>
-                <Marquee>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, distinctio!</Marquee>
+                <Marquee speed={100} pauseOnHover={true}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, distinctio!.... Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat quo vitae similique accusantium error. Fuga eaque ut molestias velit harum!
+                </Marquee>
             </div>
+            <Navbar collapseOnSelect expand="lg" bg='light' variant='light' className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                            <Nav.Link href="#features">Home</Nav.Link>
+                            <Nav.Link href="#pricing">About</Nav.Link>
+                            <Nav.Link href="#deets">Career</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            
+                            <Nav.Link eventKey={2} href="#memes">   
+                            <Button variant="secondary">Login</Button>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </Container>
     );
 };
--- src/Pages/Shared/RightNav/RightNav.jsx
+++ src/Pages/Shared/RightNav/RightNav.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 const RightNav = () => {
     return (
         <div>
-            <p>right nav</p>
+            
         </div>
     );
 };
