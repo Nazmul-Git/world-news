8564d1c5eb090ac6b1bcb8453fcd43091ed7bb46
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-09-11 04:35:42 +0600

    Implement nav right site with some list of react icon and images.

--- src/Pages/Shared/RightNav/RightNav.jsx
+++ src/Pages/Shared/RightNav/RightNav.jsx
@@ -1,11 +1,31 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub,FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import QZone from '../QZone/QZone';
+import bg from '../../../assets/bg.png';
 
 const RightNav = () => {
     return (
         <div>
-            
+            <p>Login with</p>
+            <Button className='mb-2' variant="secondary"><FcGoogle /> Login with Google </Button>
+            <Button variant="secondary"><FaGithub /> Login with Github</Button>
+            <div>
+                <h4>Find us on</h4>
+                <ListGroup>
+                    <ListGroup.Item><FaFacebook/> Facebook</ListGroup.Item>
+                    <ListGroup.Item><FaLinkedin/> Linkedin</ListGroup.Item>
+                    <ListGroup.Item><FaGithub/> Github</ListGroup.Item>
+                    <ListGroup.Item><FaTwitter/> Twitter</ListGroup.Item>
+                    <ListGroup.Item><FaInstagram/> Instagram</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <QZone></QZone>
+            <div>
+                <img src={bg} alt="" />
+            </div>
         </div>
     );
 };
