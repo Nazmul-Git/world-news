aa36046dfa711b048abc4c874a214d57a6fe6217
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-09-11 00:50:30 +0600

    Install firebase.

---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqWJF-lv5zujIBH8AkaTDWR-Sq8V8roa4",
+  authDomain: "world-news-44de6.firebaseapp.com",
+  projectId: "world-news-44de6",
+  storageBucket: "world-news-44de6.appspot.com",
+  messagingSenderId: "536829202678",
+  appId: "1:536829202678:web:08ee18f5ee6f94bdfc1cd7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
