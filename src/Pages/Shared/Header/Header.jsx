53da464b388e94da76a55cbbafeb15c0c3795104
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-09-11 02:51:48 +0600

    Install react-fast-marquee & import Marquee.

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -2,15 +2,22 @@
 import React from 'react';
 import logo from '../../../assets/logo.png';
 import moment from 'moment';
+import { Button, Container } from 'react-bootstrap';
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
     return (
-        <div className='text-center'>
-            <img src={logo} alt="" />
-            <p>Journalism Without Fear or Favour</p>
-            <p>{moment().format("dddd, MMMM Do YYYY, h:mm:ss a")}</p>
-
-        </div>
+        <Container>
+            <div className='text-center'>
+                <img src={logo} alt="" />
+                <p>Journalism Without Fear or Favour</p>
+                <p>{moment().format("dddd, MMMM Do, YYYY, h:mm:ss a")}</p>
+            </div>
+            <div>
+                <Button variant='danger'>Latest</Button>
+                <Marquee>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, distinctio!</Marquee>
+            </div>
+        </Container>
     );
 };
 
