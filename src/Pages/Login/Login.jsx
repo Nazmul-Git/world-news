1310de0106eab564f6492205360f25a3e69260eb
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-09-14 00:36:49 +0600

    Add login component structure.

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,11 +1,42 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <p>login</p>
-        </div>
+        <Container className='w-25 mx-auto mt-4'>
+            <Form>
+                <h3 className='text-center'>Please Login...</h3>
+            <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                    <Form.Label column sm="2">
+                        Email
+                    </Form.Label>
+                    <Col sm="10">
+                        <Form.Control type="email" name='email' required placeholder="Email" />
+                    </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                    <Form.Label column sm="2">
+                        Password
+                    </Form.Label>
+                    <Col sm="10">
+                        <Form.Control type="password" name='password' required placeholder="Password" />
+                    </Col>
+                </Form.Group>
+                
+                <Button variant='primary' type='btn'>Login</Button>
+                <br></br>
+                <Form.Text className='text-secondary'>
+                    Don't have an account ? <Link to='/register'>Register</Link>
+                </Form.Text>
+                <Form.Text className='text-danger'>
+
+                </Form.Text>
+            </Form>
+
+        </Container>
     );
 };
 
